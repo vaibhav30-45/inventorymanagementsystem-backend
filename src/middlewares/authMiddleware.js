45d15d7eb4b2a3d53cecc801dabef5980b249b7c
vaibhav30-45 <vshrivastava0730@gmail.com>
2025-10-30 16:08:13 +0530

    login

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,4 +1,3 @@
-
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
@@ -8,12 +7,9 @@ exports.protect = async (req, res, next) => {
     if (!token) return res.status(401).json({ message: 'No token provided' });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.id);
-    if (!user) return res.status(404).json({ message: 'User not found' });
-
-    req.user = user;
+    req.user = await User.findById(decoded.id).select('-password');
     next();
-  } catch (err) {
+  } catch (error) {
     res.status(401).json({ message: 'Unauthorized' });
   }
 };
